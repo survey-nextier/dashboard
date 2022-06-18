42758f4ab9a6f26508f0eb2a6c354448fd3367ba
survey-nextier <107775345+survey-nextier@users.noreply.github.com>
2022-06-18 23:52:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 sheet = "https://drive.google.com/uc?id=1WKbXvZDqDQmRdDjM09t5SZbB3XxFpq2s"
 
 df = pd.read_csv(sheet)
-df.fillna(" ", inplace = True)
+
 
 
 st.write("Media Mapping")
