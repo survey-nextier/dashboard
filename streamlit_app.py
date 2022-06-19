be38cd38f671221a55e9034db7e20561cded37a8
survey-nextier <107775345+survey-nextier@users.noreply.github.com>
2022-06-19 02:07:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,64 @@ import streamlit as st
 from st_aggrid import AgGrid
 import pandas as pd
 
-sheet = "https://drive.google.com/uc?id=1WKbXvZDqDQmRdDjM09t5SZbB3XxFpq2s"
+import numpy as np
+
+sheet = "https://drive.google.com/uc?id=1O6-TKQMcHlMbo5mTvW9HH_t0QbEsocO1"
 
 df = pd.read_csv(sheet)
 
 
 
-st.write("Media Mapping")
 
-df
+
+st.set_page_config(page_title="Unicef Media Mapping", page_icon=":radio:", layout="wide")
+
+'''
+# Media Mapping
+Brief description about this section
+'''
+
+# df.columns
+
+left_column, right_column = st.columns(2)
+
+left_column.selectbox("Select State", df['MAIN STATE'].unique().sort())
+
+right_column.selectbox("Select Media Type", df['MEDIA TYPE'].unique())
+
+
+
+cols = ['MEDIA OUTLET', 'MEDIA TYPE',  'ESTIMATED AUDIENCE SIZE', 'REGION', 'MAIN STATE', 'MAIN STATE %', 'MALE', 'FEMALE', 'CHILDREN (7 - 12)', 'TEENS (13 - 17)', 'YOUTH/YOUNG ADULTS (18 - 29)', 'MIDDLE AGED ADULTS (30 - 44)', 'OLDER ADULTS', 'UPPER CLASS', 'UPPER MIDDLE CLASS', 'LOWER MIDDLE CLASS', 'LOWER CLASS', 'BELOW SUBSISTENCE' 
+    , 'MARRIED', 'SINGLE', 'PRIMARY ORLOWER', 'HIGH/SECONDARY SCHOOL', 'DIPLOMA/NCE', '   DEGREE, HND OR HIGHER'  
+]
+
+df_1 = df[cols]
+
+AgGrid(df_1)
+
+
+'''
+## Section Two
+Brief description of this section
+'''
+
+
+cols = ['MEDIA OUTLET', 'MEDIA TYPE', 'N30,000 OR LESS _ _',  'N30,000 TO N50,000 _ _', 'N50,000 TO N100,000 _ _', 'N100,000 TO N200,000 _', 'N200,000 TO N500,000 _', "   DON'T KNOW/REFUSED", "CHRISTIANITY",  "ISLAM", "OTHERS RELIGION"]
+
+df_2 = df[cols]
+
+AgGrid(df_2)
+
+
+'''
+## Section Three
+Brief description of this section
+'''
+
+
+cols = ['MEDIA OUTLET', 'MEDIA TYPE', 'WORKING', 'SELF -MPLOYED', 'STUDENT/APPRENTICE', 'RETIRED/ FULL HOUSEWIFE', 'UNEMPLOYED'  ]
+
+df_3 = df[cols]
+
+AgGrid(df_3)
+
